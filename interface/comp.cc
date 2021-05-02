e78f0eb9b02327ce4ea43297693629e3f421c695
luke <luketao@icloud.com>
2021-05-01 21:51:05 -0400

    resolve ordering conflict and finalize file encode stream

--- interface/comp.cc
+++ interface/comp.cc
@@ -1,38 +1,43 @@
 #include "./comp.h"
 
+    
 Comp::Comp(int c, char **argv) {
-  if (c < 2 || c > 4) throw Error("improper usage," + info);
-
-  if (string(argv[1]) == "-decode") {
-    encode = false;
-  } else if (string(argv[1]) != "-encode") {
-    throw Error("invalid mode selected," + info + " , ex: ./CompressIt -encode -bmr < test.txt ");
+  string info = "follow format ./CompressIt <mode> <transformation> < inputflow";
+  if (c < 2 || c > 4)
+      throw Error("improper usage," + info);
+  if (string(argv[1]) == "-decode"){
+      encode = false;
+  }else if (string(argv[1]) != "-encode"){
+      throw Error("invalid mode selected," + info + " , ex: ./CompressIt -encode -bmr < test.txt ");
   }
-
-  if (encode) {
-    int counter = 1;
-    while(argv[2][counter] != 0){
-      if (argv[2][counter] == 'b')
-        Tseq.push_back(Transformation::BWT);
-      else if (argv[2][counter] == 'm')
-        Tseq.push_back(Transformation::MTF);
-      else if (argv[2][counter] == 'r')
-        Tseq.push_back(Transformation::RLE);
-      else
-        throw Error("invalid transformation selected," + info);
-
-      counter++;
-    }
+  if (encode){
+      int counter = 1;
+      while(argv[2][counter] != 0){
+          if (argv[2][counter] == 'b')
+              Tseq.push_back(Transformation::BWT);
+          else if (argv[2][counter] == 'm')
+              Tseq.push_back(Transformation::MTF);
+          else if (argv[2][counter] == 'r')
+              Tseq.push_back(Transformation::RLE);
+          else throw Error("invalid transformation selected," + info);
+          counter++;
+      }
   }
 
   int counter = 2;
   while(counter < c && argv[counter][0] != '-'){ //for future, if files are read in
-    fileName.append(string(argv[counter]));
+      fileNames.push_back(string(argv[counter++]));
   }
 }
 
 void Comp::run() {
-  unique_ptr<Input> input{new Stdin(encode)}; //create input (for now from std::cin)
+  Input *rawptr = nullptr;
+  if (fileNames.empty()){ //decide between from input stream or from file stream
+      rawptr = new Stdin(encode);
+  }else{
+      rawptr = new Ifile(encode, fileNames);
+  }
+  unique_ptr<Input> input{rawptr}; 
   std::vector<std::unique_ptr<Block> > &data = input->getInputData(); //read the data
   if (encode){ //encoding data
       Transform *encoding = new Huff(nullptr); //generate transformation sequence
